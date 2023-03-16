af9abd6e63544f1f7d7c30a1123984f201dccf51
Samuel Silva <samuel7silva@outlook.com>
2023-03-16 15:16:50 -0300

    config persistgate

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { WrappedApp } from './App';
-import store from './redux/store';
+import store, { persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <WrappedApp />
+      <PersistGate persistor={persistor}>
+        <WrappedApp />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
