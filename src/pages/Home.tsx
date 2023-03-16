1206ac56af4e2d1e942c1dba3ea5155a613306fd
Samuel Silva <samuel7silva@outlook.com>
2023-03-16 15:22:38 -0300

    test redux-saga-persist

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { botaoClicadoRequest } from '../redux/slice';
 
 function Home() {
+  const test = useSelector((state: any) => state.botaoClicado);
+  const dispatch = useDispatch();
+  function handleButtonClick() {
+    dispatch(botaoClicadoRequest());
+  }
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-slate-900">
       <h1 className="text-slate-50 text-8xl">Welcome to my Home Page!</h1>
@@ -10,6 +17,14 @@ function Home() {
       <Link to="/login" className="text-slate-50 underline pt-6">
         Login
       </Link>
+      <button
+        type="button"
+        onClick={handleButtonClick}
+        className="bg-white text-black font-semibold px-4 py-1 rounded mt-4 hover:bg-slate-400 transition ease-in-out duration-150"
+      >
+        Botão
+      </button>
+      <p className="text-slate-50 pt-9">{test ? 'Clicado' : 'Não clicado'}</p>
     </div>
   );
 }
