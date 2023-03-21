5ad9563f04a69264ca350782ae99c8ad5fd5a358
Samuel Silva <samuel7silva@outlook.com>
2023-03-21 16:02:20 -0300

    update PrivateRoute

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,18 +1,3 @@
-// import React, { FC } from 'react';
-// import { Route, RouteProps, Navigate } from 'react-router-dom';
-
-// interface PrivateRouteProps extends RouteProps {
-//   isLoggedIn: boolean;
-// }
-
-// // eslint-disable-next-line react/function-component-definition, no-undef
-// const PrivateRoute: FC<PrivateRouteProps> = ({ isLoggedIn, ...rest }) => {
-//   // eslint-disable-next-line react/jsx-props-no-spreading
-//   return isLoggedIn ? <Route {...rest} /> : <Navigate to="/login" replace />;
-// };
-
-// export default PrivateRoute;
-
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
