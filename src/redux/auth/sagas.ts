ac4d0a08b39f68f80075ca4a99a571802f9cbdb3
Samuel Silva <samuel7silva@outlook.com>
2023-03-21 12:20:30 -0300

    update sagas

---
+++ src/redux/auth/sagas.ts
@@ -0,0 +1,26 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import { loginRequest, loginSuccess, loginFailure } from './slice';
+import axios from '../../services/axios';
+
+function* login({ payload }: { payload: any }): Generator<any, any, any> {
+  try {
+    const response = yield call(axios.post, '/tokens/', payload);
+    // console.log(response.data);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+
+    yield put(loginSuccess(response.data));
+    toast.success('Usuário logado!');
+    console.log('This login');
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+  } catch (e) {
+    toast.error('Usuário ou senha inválidos!');
+    yield put(loginFailure(payload));
+    console.log(e);
+  }
+}
+
+export default all([takeLatest(loginRequest, login)]);
+
+// cold77man@gmail.com
