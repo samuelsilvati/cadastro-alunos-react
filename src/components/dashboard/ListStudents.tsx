4cabc934c338839397da9a494d5029e32939fca3
Samuel Silva <samuel7silva@outlook.com>
2023-03-25 21:28:56 -0300

    configure list student component

--- src/components/dashboard/ListStudents.tsx
+++ src/components/dashboard/ListStudents.tsx
@@ -1,29 +1,71 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { get } from 'lodash';
+import { User } from '@phosphor-icons/react';
+import axios from '../../services/axios';
+import LoadingComponent from './LoadingComponent';
+
+interface Student {
+  id: number;
+  nome: string;
+  sobrenome: string;
+  email: string;
+  Images: Image[];
+}
+
+interface Image {
+  url: string;
+}
 
 function ListStudents() {
+  const [students, setStudents] = useState<Student[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function getData() {
+      const response = await axios.get('/alunos');
+      setStudents(response.data);
+      setIsLoading(false);
+    }
+    getData();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingComponent isLoading={isLoading} />;
+  }
   return (
-    <div>
+    <div className="overflow-y-auto h-[750px]">
       <div className="border-b-2 border-zinc-100 pb-4 pl-5 text-lg font-semibold">
         <p>Nome</p>
       </div>
-      <Link to="/dashboard">
-        <div className="flex items-center gap-3 my-4 py-2 pl-5 border-b-2 border-zinc-100 hover:rounded-lg hover:bg-zinc-100 transition ease-in-out duration-400">
-          <div className="rounded-full w-16 h-16 bg-[url('/src/img/avatar.jpg')] bg-cover" />
-          <div>
-            <p className="text-lg font-semibold">Samuel Silva</p>
-            <p className="text-sm font-semibold">samuel7silva@outlook.com</p>
+      {students.map((student) => (
+        <Link to={`/dashboard/${student.id}/edit`} key={student.id}>
+          <div
+            key={student.id}
+            className="flex items-center gap-3 my-4 py-2 pl-5 border-b-2 border-zinc-100 hover:rounded-lg hover:bg-zinc-100 transition ease-in-out duration-400"
+          >
+            {get(student, 'Images[0].url', false) ? (
+              <div
+                className="rounded-full w-16 h-16 bg-cover bg-center"
+                style={{ backgroundImage: `url(${student?.Images?.[0]?.url})` }}
+              />
+            ) : (
+              <div className="rounded-full w-16 h-16 bg-zinc-300 flex justify-center">
+                <div>
+                  <User size={50} className="pt-2 m-0 text-zinc-400" />
+                </div>
+              </div>
+            )}
+            <div>
+              <p className="text-lg font-semibold">
+                {student.nome} {student.sobrenome}
+              </p>
+              <p className="text-sm font-semibold">{student.email}</p>
+            </div>
           </div>
-        </div>
-      </Link>
-      <Link to="/dashboard">
-        <div className="flex items-center gap-3 my-4 py-2 pl-5 border-b-2 border-zinc-100 hover:rounded-lg hover:bg-zinc-100 transition ease-in-out duration-400">
-          <div className="rounded-full w-16 h-16 bg-[url('/src/img/avatar0.jpg')] bg-cover" />
-          <div>
-            <p className="text-lg font-semibold">Joana Doe</p>
-            <p className="text-sm font-semibold">annadoe@gmail.com</p>
-          </div>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </div>
   );
 }
